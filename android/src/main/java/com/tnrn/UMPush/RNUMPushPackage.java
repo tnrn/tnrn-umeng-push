953a263cf246e12abe0e139427385092e9980993
zhangjingzhu <251801008@qq.com>
2019-02-27 14:00:00 +0800

    Add 添加原生友盟推送SDK

--- android/src/main/java/com/tnrn/UMPush/RNUMPushPackage.java
+++ android/src/main/java/com/tnrn/UMPush/RNUMPushPackage.java
@@ -1,28 +1,45 @@
 
 package com.tnrn.UMPush;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
-public class RNUMPushPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNUMPushModule(reactContext));
-    }
 
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
+/**
+ * Created by wangfei on 17/8/28.
+ */
+
+public class RNUMPushPackage implements ReactPackage {
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Collections.emptyList();
+    }
+
+    /**
+     * 如需要添加本地方法，只需在这里add
+     *
+     * @param reactContext
+     * @return
+     */
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        ArrayList<NativeModule> modules = new ArrayList<NativeModule>();
+        modules.add(new RNUMPushModule(reactContext));
+        return modules;
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
     }
 }
